79ac0d0f15bdc5897857a54c08988e7b144a4896
dani <danigarridonunez@gmail.com>
2024-03-10 19:44:11 +0100

    Agregado el controlador de las paginas genericas

---
+++ controllers/PaginasController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+use Model\Propiedad;
+
+class PaginasController {
+    public static function index( Router $router ) {
+        
+        $propiedades = Propiedad::get(3);
+        $inicio = true;
+
+        $router->render('paginas/index', [
+            'propiedades' => $propiedades,
+            'inicio' => $inicio
+        ]);
+    }
+
+    public static function nosotros( Router $router ) {
+        
+        $router->render('paginas/nosotros', []);
+    }
+
+    public static function propiedades( Router $router ) {
+        
+        $propiedades = Propiedad::all();
+
+        $router->render('paginas/propiedades', [
+            'propiedades' => $propiedades
+        ]);
+    }
+    
+    public static function propiedad( Router $router ) {
+        echo "Desde propiedad";
+    }
+
+    
+    public static function blog( Router $router ) {
+        echo "Desde Blog";
+    }
+
+    
+    public static function entrada( Router $router ) {
+        echo "Desde Entrada";
+    }
+
+    
+    public static function contacto( Router $router ) {
+        echo "Desde Contacto";
+    }
+}
